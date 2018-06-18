b09608a30a00415bca3487a1dfaaaf44bdbe40e5
anton <antontishchenko@gmail.com>
2018-06-18 10:43:47 +0300

    Initial commit: project structure

---
+++ CDN.Akamai.DeviceDetection/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDN.Akamai.DeviceDetection
+{
+    public static class Extensions
+    {
+        public static Dictionary<string, string> ParseAkamaiHeader(this string headerValue)
+        {
+            var pairs = headerValue.Split(',').Select(x => x.Trim());
+            var dictionary = new Dictionary<string, string>();
+            foreach (var pair in pairs)
+            {
+                var parts = pair.Split('=');
+                if (parts.Length > 1)
+                {
+                    var key = parts[0];
+                    var value = parts[1];
+                    dictionary.Add(key, value);
+                }
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ CDN.Akamai.DeviceDetection/GeoIP/WhoIsInformation.cs
@@ -0,0 +1,18 @@
+﻿namespace CDN.Akamai.DeviceDetection.GeoIP
+{
+    public class WhoIsInformation
+    {
+        public string AreaCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Dns { get; set; }
+        public string Isp { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string MetroCode { get; set; }
+        public string Name { get; set; }
+        public string PostalCode { get; set; }
+        public string Region { get; set; }
+        public string Url { get; set; }
+    }
+}
