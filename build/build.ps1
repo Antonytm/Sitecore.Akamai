b33819e8c5cdd6e31071506bfe2e179986618a60
Antonytm <antontishchenko@gmail.com>
2018-07-01 14:02:14 +0300

    Collect versioned artifacts

--- build/build.ps1
+++ build/build.ps1
@@ -5,10 +5,14 @@ if ($version -eq $null) {
 "Package version: " + $version
 
 Remove-Item build\package -Recurse -ErrorAction Ignore
+Remove-Item build\artifacts -Recurse -ErrorAction Ignore
 New-Item -Name build\package -ItemType directory
+New-Item -Name build\artifacts -ItemType directory
 New-Item -Name build\package\bin -ItemType directory
 New-Item -Name build\package\App_Config\Include\Foundation -ItemType directory
+
 Copy-Item .\src\Foundation\Akamai\code\bin\Foundation.Akamai* .\build\package\bin
 Copy-Item .\src\Foundation\Akamai\code\App_Config\Include\Foundation\Foundation.Sitecore.Akamai.config .\build\package\App_Config\Include\Foundation
-$packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\sitecore.akamai." + $version + ".update -r"
+
+$packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\artifacts\sitecore.akamai." + $version + ".update -r"
 iex $packageCmd
