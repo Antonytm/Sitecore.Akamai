9b3c3e52e53d0f17f7bbc05d20b96d5389b0683f
Antonytm <antontishchenko@gmail.com>
2018-07-01 13:39:08 +0300

    Attempt to build a package

---
+++ build/build.ps1
@@ -0,0 +1,14 @@
+$version = $env:PACKAGE_VERSION.$env.APPVEYOR_BUILD_NUMBER
+if ($version -eq $null) {
+    $version = "1.0.0.0"
+}
+"Package version: " + $version
+
+Remove-Item build\package -Recurse -ErrorAction Ignore
+New-Item -Name build\package -ItemType directory
+New-Item -Name build\package\bin -ItemType directory
+New-Item -Name build\package\App_Config\Include\Foundation -ItemType directory
+Copy-Item .\src\Foundation\Akamai\code\bin\Foundation.Akamai* .\build\package\bin
+Copy-Item .\src\Foundation\Akamai\code\App_Config\Include\Foundation\Foundation.Sitecore.Akamai.config .\build\package\App_Config\Include\Foundation
+$packageCmd = "Sitecore.Courier.Runner.exe -t build\package -o build\sitecore.akamai." + $version + ".update -r"
+iex $packageCmd
