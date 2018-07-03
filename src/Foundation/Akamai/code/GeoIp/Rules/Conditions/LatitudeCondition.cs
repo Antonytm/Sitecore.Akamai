b3436b668ac4907a27f28ccdbf8dfa689e902840
Antonytm <antontishchenko@gmail.com>
2018-07-03 22:30:48 +0300

    Latitude and longitude conditions

---
+++ src/Foundation/Akamai/code/GeoIp/Rules/Conditions/LatitudeCondition.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Foundation.Akamai.GeoIp.Rules.Conditions
+{
+    public class LatitudeCondition<T> : IntegerComparisonCondition<T> where T : RuleContext
+    {
+        private HeaderParser HeaderParser => new HeaderParser();
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            var valueToCompare = HeaderParser.GetStringValue(Const.GeoIpProperties.Latitude);
+            return base.Compare(int.Parse(valueToCompare));
+        }
+    }
+}
---
+++ src/Foundation/Akamai/code/GeoIp/Rules/Conditions/LongitudeCondition.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Foundation.Akamai.GeoIp.Rules.Conditions
+{
+    public class LongitudeCondition<T> : IntegerComparisonCondition<T> where T : RuleContext
+    {
+        private HeaderParser HeaderParser => new HeaderParser();
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            var valueToCompare = HeaderParser.GetStringValue(Const.GeoIpProperties.Longitude);
+            return base.Compare(int.Parse(valueToCompare));
+        }
+    }
+}
